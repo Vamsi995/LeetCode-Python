3a691c575f597470788b970423dc7914668222f1
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2022-09-02 12:42:58 +0530

    Time: 628 ms (85.75%), Space: 27.7 MB (82.36%) - LeetHub

---
+++ 2214-minimum-health-to-beat-game/2214-minimum-health-to-beat-game.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumHealth(self, damage: List[int], armor: int) -> int:
+       
+        s = 0
+        
+        max_value = 0
+        
+        for i in range(len(damage)):
+            
+            if damage[i] > max_value:
+                max_value = damage[i]
+                
+            s += damage[i]
+            
+        if max_value > armor:
+            rem = max_value - armor
+            s -= max_value 
+            s += rem
+        else:
+            s -= max_value
+        
+        return s + 1
+    
