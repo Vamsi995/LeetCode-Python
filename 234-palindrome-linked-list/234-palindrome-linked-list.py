a9f5f498a2e7630c5551f3e1a978a835e2cc6cc5
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2022-09-08 15:36:50 +0530

    Time: 3000 ms (5.02%), Space: 131.3 MB (5.00%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -3,49 +3,18 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+one_way = ""
+other_way = ""
 class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        
+    def isPalindrome(this, head):
+        global one_way
+        global other_way
+        one_way = one_way + str(head.val)
+        other_way  = str(head.val) + other_way
+        # print(one_way, other_way)
         if head.next == None:
-            return True
-        
-        slow = head
-        fast = head
-        
-        prev = None
-        nex = slow.next
-        count = 0
-        while fast != None and fast.next != None:
-            
-            fast = fast.next.next
-            slow.next = prev
-            prev = slow
-            slow = nex
-            nex = nex.next
-            count += 1
-            
-        
-        l_head = prev
-        
-        temp = slow
-        c = 0
-        while temp != None:
-            c += 1
-            temp = temp.next
-            
-        r_head = None
-        if (c + count) % 2 == 0:
-            r_head = slow
-        else:
-            r_head = slow.next
-
-        
-        while l_head != None and r_head != None:
-            
-            if l_head.val != r_head.val:
-                return False
-            
-            l_head = l_head.next
-            r_head= r_head.next
-        
-        return True
+            ans = one_way == other_way
+            one_way = ""
+            other_way = ""
+            return ans
+        return this.isPalindrome(head.next)
