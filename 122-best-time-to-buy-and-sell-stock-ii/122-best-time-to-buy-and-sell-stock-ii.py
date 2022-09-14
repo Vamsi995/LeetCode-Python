9782461f3f26317d846170e1c0bc116da34b9e0e
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2022-09-14 16:12:35 +0530

    Time: 127 ms (25.36%), Space: 15.1 MB (97.77%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.py
@@ -5,18 +5,9 @@ class Solution:
         min_price = float("Inf")
         profit = 0
         
-        for i in range(len(prices)):
+        for i in range(1, len(prices)):
             
-            min_price = min(min_price, prices[i])
-            
-            if i == len(prices) - 1:
-                profit += prices[i] - min_price
-                break
-            
-            if prices[i+1] - prices[i] < 0:
-                profit += prices[i] - min_price
-                min_price = float("Inf")
-            
-            
+            if prices[i] - prices[i-1] > 0:
+                profit += prices[i] - prices[i-1]
+                
         return profit
-        
