726b691d550f0b1429f2d15b165899f6370e42b6
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2022-10-07 19:51:34 +0530

    Time: 2147 ms (30.05%), Space: 14.6 MB (91.38%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.py
+++ 731-my-calendar-ii/731-my-calendar-ii.py
@@ -1,21 +1,34 @@
-class MyCalendarTwo(object):
+class MyCalendarTwo:
 
     def __init__(self):
-        self.lst =[]
-
-    
-    def book (self, start, end):
-        self.lst.append((start, 1))
-        self.lst.append((end, -1))
         
-        self.lst.sort()
-        overlaps = 0
-        for b in self.lst:
-            overlaps+=b[1]
-            if overlaps >2:
-                self.lst.remove((start, 1))
-                self.lst.remove((end, -1))
+        self.bookings = []
+
+    def book(self, start: int, end: int) -> bool:
+        
+        # self.bookings[start] = 1 + self.bookings.get(start, 0)
+        # self.bookings[end] = -1 + self.bookings.get(end, 0)
+        self.bookings.append((start, 1))
+        self.bookings.append((end, -1))
+        count = 0
+        # print(start, end, self.bookings[(start, (start, end))])
+        self.bookings.sort()
+        
+        for c, val in self.bookings:
+                
+            count += val
+            
+            if count >= 3:
+                self.bookings.remove((start, 1))
+                self.bookings.remove((end, -1))
+                
+#                 if self.bookings[start] == 0:
+#                     self.bookings.pop(start)
+                    # self.bookings.pop(end)
+                
                 return False
+        
+        
         return True
         
 
