f6e71393f4a79b9810644f784cf6cd14cf8bf6b8
Sai Vamsi Alisetti <52487689+Vamsi995@users.noreply.github.com>
2022-10-08 10:49:33 +0530

    Time: 4452 ms (85.45%), Space: 14.1 MB (53.95%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,3 +1,31 @@
+# class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        
+#         nums.sort()
+#         result = inf
+#         stored_sum = None
+        
+#         for i in range(len(nums)):
+            
+#             l = i + 1
+#             r = len(nums) - 1
+            
+#             while l < r:
+                
+#                 tot = nums[i] + nums[l] + nums[r]
+#                 dif = abs(target - tot) 
+#                 if dif < result:
+#                     result = dif
+#                     stored_sum = tot
+#                 if tot == target:
+#                     return tot
+#                 elif tot < target:
+#                     l += 1
+#                 elif tot > target:
+#                     r -= 1
+                
+#         return stored_sum
+
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
